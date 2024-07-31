927865bbd20a3503edc652bae3edf79bf8f36fed
Nix <75538775+nixietab@users.noreply.github.com>
2024-07-31 02:34:54 -0300

    made responsive

--- girl.py
+++ girl.py
@@ -37,6 +37,7 @@ def generate_html(info_dict):
 <html>
 <head>
     <title>GIRL</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
     <script src="script.js" defer></script>
     <link rel="icon" type="image/x-icon" href="favicon.png">
